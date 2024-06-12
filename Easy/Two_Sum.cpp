c491e7e7cde460f63b05ac5ac0c738c88e193097
CHUL SEUNG LEE <fpsemfaos@gmail.com>
2024-06-12 01:45:12 -0400

    Two Sum Problem

---
+++ Easy/Two_Sum.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+/*      Two Sum
+ Given an array of integers nums and an integer target, return indices of the
+two numbers such that they add up to target. You may assume that each input
+would have exactly one solution, and you may not use the same element twice. You
+can return the answer in any order.
+
+Example 1:
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+    Input: nums = [3,2,4], target = 6
+    Output: [1,2]
+Example 3:
+    Input: nums = [3,3], target = 6
+    Output: [0,1] */
+
+class MySolution {
+   public:
+    /* Time Complexity: O(n^2)
+       Space Complexity: O(1) */
+    vector<int> twoSum(vector<int> &nums, int target) {
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = nums.size() - 1; j > i; --j) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
+
+class BetterSolution {
+   public:
+    /* Time Complexity: O(n)
+       Space Complexity: O(n) */
+    vector<int> twoSum(vector<int> &nums, int target) {
+        unordered_map<int, int> hash_map;
+        for (int i = 0; i < nums.size(); ++i) {
+            int diff = target - nums[i];
+            if (hash_map.find(diff) != hash_map.end()) {
+                return {hash_map[diff], i};
+            }
+            hash_map[nums[i]] = i;
+        }
+        return {};
+    };
+};
